c3fbcfa54a7fc9f3d748d657e5da13feabf1d516
manuelatrotta <manuelat88@hotmail.it>
2019-12-18 15:33:00 +0100

    inserimento km da percorrere

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 alert("click ok for visit this page!");
 //chiedere i km da percorrere
+var kmPercorso = parseInt (prompt ('Inserire i km da percorrere'));
+console .log (kmPercorso);
 //chiedere età
 //moltiplicare km da percorrere con prezzo al km per avere prezzo biglietto
 //vedere se età<=18 applicare sconto 20%

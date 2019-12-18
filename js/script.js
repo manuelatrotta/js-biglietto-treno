e97a2f5880fa49ac1dcb2f6703e4537bf89325d0
manuelatrotta <manuelat88@hotmail.it>
2019-12-18 17:05:15 +0100

    aggiunta prezzo in html

--- js/script.js
+++ js/script.js
@@ -2,21 +2,25 @@ alert("click ok for visit this page!");
 //chiedere i km da percorrere
 var kmPercorso = parseInt (prompt ('Inserire i km da percorrere'));
 console .log (kmPercorso);
+document .getElementById('kmPercorso') .innerHTML = "Secondo i tuoi km da percorrere:" + kmPercorso;
 //chiedere età
 var etaUtente = parseInt (prompt ('Inserire la tua età'));
 console .log (etaUtente);
 //moltiplicare km da percorrere con prezzo al km per avere prezzo biglietto
 var prezzoBiglietto = kmPercorso * 0.21;
 console .log (prezzoBiglietto);
+document .getElementById('prezzoBiglietto') .innerHTML = "Il prezzo del tuo biglietto secondo tariffa ordinaria é:" + prezzoBiglietto + "&euro;";
 //vedere se età<=18 applicare sconto 20%
 var scontoMinorenni = (prezzoBiglietto* 20 / 100);
 var prezzoMinorenni = (prezzoBiglietto - scontoMinorenni);
-if (etaUtente <=18) {
+if (etaUtente <18) {
   console .log (prezzoMinorenni);
+  document .getElementById('prezzoMinorenni') .innerHTML = "Essendo minorenne puoi usufruire del prezzo ridotto " + prezzoMinorenni + "&euro;";
 }
 // se età >=65 applicare sconto 40%
 var scontoOver = (prezzoBiglietto* 40 / 100);
 var prezzoOver = (prezzoBiglietto - scontoOver);
 if (etaUtente >=65) {
   console .log (prezzoOver);
+  document .getElementById('prezzoOver') .innerHTML = "Essendo Over65 puoi usufruire del prezzo ridotto " + prezzoOver + "&euro;";
 }

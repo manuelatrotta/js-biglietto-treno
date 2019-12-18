db13dbd3c9565fc124298600bd3eff21048f977a
manuelatrotta <manuelat88@hotmail.it>
2019-12-18 15:22:28 +0100

    analisi problema

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 alert("click ok for visit this page!");
+//chiedere i km da percorrere
+//chiedere età
+//moltiplicare km da percorrere con prezzo al km per avere prezzo biglietto
+//vedere se età<=18 applicare sconto 20%
+// se età >=65 applicare sconto 40%

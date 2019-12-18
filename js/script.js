549bbded7ad0314e1898bf63e7c315d917b33d56
manuelatrotta <manuelat88@hotmail.it>
2019-12-18 15:34:15 +0100

    inserimento età utente

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ alert("click ok for visit this page!");
 var kmPercorso = parseInt (prompt ('Inserire i km da percorrere'));
 console .log (kmPercorso);
 //chiedere età
+var etaUtente = parseInt (prompt ('Inserire la tua età'));
+console .log (etaUtente);
 //moltiplicare km da percorrere con prezzo al km per avere prezzo biglietto
 //vedere se età<=18 applicare sconto 20%
 // se età >=65 applicare sconto 40%

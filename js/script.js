f0e1df60c6de77f77b73f90cfeaaf147c1b7c23b
manuelatrotta <manuelat88@hotmail.it>
2019-12-26 17:54:36 +0100

    inserimento if

--- js/script.js
+++ js/script.js
@@ -7,40 +7,27 @@ function(){
   console.log(nomeUtente);
 
   var inputKm = document.getElementById('km');
-  var kmDaPercorrere = inputKm.value;
+  var kmDaPercorrere = parseInt(inputKm.value);
   console.log(kmDaPercorrere);
 
   var rangeEtaSelect = document.getElementById('range-eta');
   var fascia = rangeEtaSelect.value;
   console.log(fascia);
-}
-)
-//  var inputKm
-//})
-//chiedere i km da percorrere
-//var kmPercorso = parseInt (prompt ('Inserire i km da percorrere'));
-//console.log(kmPercorso);
-//document.getElementById('kmPercorso').innerHTML="Secondo i tuoi km da percorrere:" + kmPercorso;
-//chiedere età
-//var etaUtente=parseInt(prompt ('Inserire la tua età'));
-//console.log(etaUtente);
-//moltiplicare km da percorrere con prezzo al km per avere prezzo biglietto
-//var prezzoBiglietto=kmPercorso * 0.21;
-//console.log(prezzoBiglietto);
-//document.getElementById('prezzoBiglietto') .innerHTML = "Il prezzo del tuo biglietto secondo tariffa ordinaria é:" + prezzoBiglietto + "&euro;";
-//vedere se età<=18 applicare sconto 20%
-//var scontoMinorenni=(prezzoBiglietto* 20 / 100);
-//var prezzoMinorenni=(prezzoBiglietto - scontoMinorenni);
-// se età >=65 applicare sconto 40%
-//if(etaUtente <18){
-  //var scontoMinorenni=(prezzoBiglietto* 20 / 100);
-  //var prezzoMinorenni=(prezzoBiglietto - scontoMinorenni);
-  //document.getElementById('prezzoMinorenni').innerHTML="Essendo minorenne puoi usufruire del prezzo ridotto " + prezzoMinorenni + "&euro;";
-//}else if (etaUtente >=65) {
-  //var scontoOver = (prezzoBiglietto* 40 / 100);
-  //var prezzoOver = (prezzoBiglietto - scontoOver);
-  //  document.getElementById('prezzoOver').innerHTML="Essendo Over65 puoi usufruire del prezzo ridotto " + prezzoOver + "&euro;";
-//}
-//else {
-//  document.getElementById('prezzoBiglietto');
-//}
+
+  var prezzoKm = 0.21;
+  var prezzoBiglietto = kmDaPercorrere * prezzoKm;
+  console.log(prezzoBiglietto);
+
+  var offerta = 'Tariffa standard';
+
+
+    if(fascia=='minorenne'){
+    prezzoBiglietto -= ((prezzoBiglietto * 20) / 100);
+
+    } else if(fascia=='Over65'){
+      prezzoBiglietto -= ((prezzoBiglietto * 40) / 100);
+    }
+    document.getElementById('nomeUtente').innerHTML = name;
+    document.getElementById('offerta').innerHTML = offerta;
+  }
+);
